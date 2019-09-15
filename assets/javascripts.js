d0c83441cddf7645a32788d7d92b594337826c76
Abdulsalam <salwom@live.com>
2019-09-15 20:09:11 +0300

    مرحلة الانتهاء من اللعبه

---
+++ assets/javascripts.js
@@ -0,0 +1,184 @@
+var boxs = document.querySelectorAll(".box");
+var timer = document.querySelector("span");
+var timerTheColor = document.querySelector("h2");
+var btn = document.querySelector(".btn");
+var textWiner = document.querySelector(".s");
+
+var firstArry = numpers(1, 13);;
+var sacendArry = numpers(14, 25);
+var mainArr = numpers(26, 50);
+var testValue = 1;
+var elapsedTimes;
+var interval;
+
+
+//---- var mainArry = firstArry.concat(sacendArry);
+var x = firstRandom();
+var xx = sacendRandom();
+var xxx = x.concat(xx);
+
+btn.addEventListener("click", function () {
+    firstArry = numpers(1, 13);;
+    sacendArry = numpers(14, 25);
+    mainArr = numpers(26, 50);
+
+    x = firstRandom();
+    xx = sacendRandom();
+    xxx = x.concat(xx);
+
+    testValue = 1;
+    for (let i = 0; i < boxs.length; i++) {
+        boxs[i].style.display = "block";
+        boxs[i].style.visibility = "visible";
+        boxs[i].style.background = "#03858B";
+    }
+
+    creatGame();
+    document.querySelector("p").textContent = "الرقم التالي : " + testValue;
+
+    clearInterval(interval);
+    timer.innerHTML = "0.000";
+    textWiner.style.display = "none";
+
+});
+
+function firstRandom() {
+    let f_14 = [];
+    for (let i = 0; i < 13; i++) {
+        let x = Math.floor(Math.random() * firstArry.length);
+        f_14.push(firstArry[x]);
+        firstArry.splice(x, 1);
+    } return f_14;
+}
+
+function sacendRandom() {
+    let f_11 = [];
+    for (let i = 0; i < 12; i++) {
+        let x = Math.floor(Math.random() * sacendArry.length);
+        f_11.push(sacendArry[x]);
+        sacendArry.splice(x, 1);
+    } return f_11;
+}
+
+creatGame();
+
+
+//ينشئ القيم الاولة للمصفوفة
+function creatGame() {
+    for (let i = 0; i < 25; i++) {
+        let y = Math.floor(Math.random() * xxx.length);
+        boxs[i].textContent = xxx[y];
+        //mainArr.push(sacendArry[y]);
+        xxx.splice(y, 1);
+        sacendArry.splice(y, 1);
+    }
+}
+
+boxs.forEach(function (box) {
+    box.addEventListener("click", function () {
+        var value = parseInt(this.textContent);
+        if (testValue === value) {
+            if (testValue === 1) {
+                bTimer();
+            }
+            testValue++;
+            endComments(testValue);
+            box.style.background = "#fff";
+            setTimeout(function () {
+                box.style.visibility = "hidden";
+            }, 250);
+
+
+            //مساعد ضهور الرقم  
+            boxs.forEach(function (box) {
+                let value = parseInt(box.textContent);
+                if (testValue === value) {
+                    box.style.background = "purple";
+                }
+            });
+
+            boxGeneratorِ(box);
+        } else {
+            for (let i = 1; i < 4; i++) {
+                timerTheColor.style.background = "red";
+                setTimeout(function () {
+                    timerTheColor.style.background = "#fff";
+                }, 400);
+            }
+        }
+    });
+});
+
+function boxGeneratorِ(box) {
+    let a = Math.floor(Math.random() * mainArr.length);
+    box.textContent = mainArr[a];
+
+    setTimeout(function () {
+        if (!mainArr.length) {
+            box.style.background = "white";
+            box.style.visibility = "hidden";
+        }
+        else {
+            box.style.visibility = "visible";
+            box.style.background = "#04ABB2";
+            mainArr.splice(a, 1);
+        }
+    }, 250);
+}
+
+
+//ادخال قم المصفوفة ****
+function numpers(index, num) {
+    var arr = [];
+    for (let i = index; i <= num; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+// sessionStorage.clear();
+//تعليقات النهائية
+function endComments(testValue) {
+    if (testValue == 51) {
+        document.querySelector("p").textContent = "مبارك عليك";
+        var strings;
+        var strings2="";
+        if (elapsedTimes < 45) {
+            strings = "ذكائك الخارق";
+        } else if (elapsedTimes < 55) {
+            strings = "العقلية الخبيره";
+        } else if (elapsedTimes < 65) {
+            strings = "هذا الذكاء المميز";
+        } else if (elapsedTimes < 85) {
+            strings = "طبيعتك العاديه";
+        } else if (elapsedTimes < 90) {
+            strings = "شيبتك المبكره";
+        } else if (elapsedTimes < 105 && elapsedTimes > 90) {
+            strings = "اشتباه حالتك بمرض الزهايمر";
+        } else {
+            strings2 = "لا تـ"
+            strings = "هذي النتيجه "
+        }
+        for (let i = 0; i < boxs.length; i++) {
+            boxs[i].style.display = "none";
+        }
+        textWiner.style.display = "block";
+        textWiner.textContent = strings2+"شارك " + strings + " مع اصحابك";
+
+    } else {
+        document.querySelector("p").textContent = "الرقم التالي : " + testValue;
+    }
+}
+
+
+// الوقت
+function bTimer() {
+    var startTime = Date.now();
+    interval = setInterval(function () {
+        var elapsedTime = Date.now() - startTime;
+        elapsedTimes = elapsedTime / 1000;
+        timer.innerHTML = (elapsedTimes).toFixed(3);
+        if (testValue == 51) {
+            clearInterval(interval);
+        }
+    }, 100);
+}
