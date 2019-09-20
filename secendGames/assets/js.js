aa8c1f47fcfc666dd4f10326a4fa0bca4035038c
Abdulsalam <salwom@live.com>
2019-09-21 00:55:55 +0300

    try

--- secendGames/assets/js.js
+++ secendGames/assets/js.js
@@ -1,6 +1,6 @@
 var squirSelctor = document.querySelectorAll(".squir");
 var imgContainer = [
-    '<i class="far fa-angry"></i>',
+    /*'<i class="far fa-angry"></i>',
     '<i class="fas fa-kaaba"></i>',
     '<i class="fas fa-bicycle"></i>',
     '<i class="fas fa-car-side"></i>',
@@ -8,25 +8,25 @@ var imgContainer = [
     '<i class="fas fa-child"></i>',
     '<i class="fas fa-dollar-sign"></i>',
     '<i class="far fa-grin-beam-sweat"></i>',
-    '<i class="far fa-heart"></i>',
+    '<i class="far fa-heart"></i>',*/
     '<i class="fas fa-horse"></i>'
 ];
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 imgContainer = imgContainer.concat(imgContainer);
+number = number.concat(number);
+for (let i = 0; i < 2; i++) {
 
-var y = 0;
-for (let x = 0; x < 10; x++) {
+    let boxImge = Math.floor(Math.random() * imgContainer.length);
 
-    for (let i = 0; i < 2; i++) {
-        let boxImge = Math.floor(Math.random() * imgContainer.length);
-        squirSelctor[y].innerHTML = imgContainer[boxImge];
-        imgContainer.splice(boxImge, 1);
-        y++;
-    }
+    let s = (squirSelctor[i].innerHTML = imgContainer[boxImge]);
+    console.log(s);
+    //squirSelctor[i].innerHTML = imgContainer[boxImge];
+    imgContainer.splice(boxImge, 1);
+    number.splice(boxImge, 1);
 }
 
-for (let i = 0; i < squirSelctor.length; i++) {
-    //squirSelctor[i].style.color = "rgb(24, 24, 24)";
-}
+
 var f;
 var s;
 var clickNo = 1;
@@ -43,7 +43,7 @@ squirSelctor.forEach(function (selectedBox1) {
             if (f == s) {
                 f.style.color = "pink";
                 s.style.color = "pink";
-            }else{
+            } else {
                 f.style.color = "rgb(24, 24, 24)";
                 s.style.color = "rgb(24, 24, 24)";
             }
@@ -52,7 +52,7 @@ squirSelctor.forEach(function (selectedBox1) {
             document.querySelectorAll
             */
         }
-        
+
     });
 
 });
