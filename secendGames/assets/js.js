c5fb768387ddf94d0ef7173cd441189d760ae63f
Abdulsalam <salwom@live.com>
2019-09-25 00:31:21 +0300

    fix the simeler

--- secendGames/assets/js.js
+++ secendGames/assets/js.js
@@ -1,112 +1,124 @@
-var squirSelctor = document.querySelectorAll(".squir");
-var imgContainer1 = [
-    /*'<i class="far fa-angry"></i>',
-    '<i class="fas fa-kaaba"></i>',
-    '<i class="fas fa-bicycle"></i>',
-    '<i class="fas fa-car-side"></i>',
-    '<i class="fas fa-cat"></i>',
-    '<i class="fas fa-child"></i>',
-    '<i class="fas fa-dollar-sign"></i>',
-    '<i class="far fa-grin-beam-sweat"></i>',
-    '<i class="far fa-heart"></i>',*/
-    '<i class="fas fa-horse"></i>'
-];
-var imgContainer2 = [
-    /*'<i class="far fa-angry"></i>',
-    '<i class="fas fa-kaaba"></i>',
-    '<i class="fas fa-bicycle"></i>',
-    '<i class="fas fa-car-side"></i>',
-    '<i class="fas fa-cat"></i>',
-    '<i class="fas fa-child"></i>',
-    '<i class="fas fa-dollar-sign"></i>',
-    '<i class="far fa-grin-beam-sweat"></i>',
-    '<i class="far fa-heart"></i>',*/
-    '<i class="fas fa-horse"></i>'
-];
-
-var number1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var number2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// imgContainer = imgContainer.concat(imgContainer);
-//number = number.concat(number);
+// window.onload = function ()
+// {
+//       Javascript code goes here
+//   } 
+window.onload = function () {
+    var surCon = document.querySelectorAll(".surCon");
+    var squirSelctor = document.querySelectorAll(".squir");
 
 
-function imgContainer1Call(i) {
-    let boxImge = Math.floor(Math.random() * imgContainer1.length);
-    squirSelctor[i].innerHTML = imgContainer1[boxImge];
-    squirSelctor[i].value = boxImge;
-    imgContainer1.splice(boxImge, 1);
-    number1.splice(boxImge, 1);
-}
-function imgContainer2Call(i) {
-    let boxImge = Math.floor(Math.random() * imgContainer2.length);
-    squirSelctor[i].innerHTML = imgContainer2[boxImge];
-    squirSelctor[i].value = boxImge;
-    imgContainer2.splice(boxImge, 1);
-    number2.splice(boxImge, 1);
-}
+    var imgContainer1 = [
+        '<i class="far fa-angry" value = 1></i>',
+        '<i class="fas fa-kaaba" value = 2></i>',
+        '<i class="fas fa-bicycle" value = 3></i>',
+        '<i class="fas fa-car-side" value = 4></i>',
+        '<i class="fas fa-cat" value = 5></i>',
+        '<i class="fas fa-child" value = 6></i>',
+        '<i class="fas fa-dollar-sign" value = 7></i>',
+        '<i class="far fa-grin-beam-sweat" value = 8></i>',
+        '<i class="far fa-heart" value = 9></i>',
+        '<i class="fas fa-horse" value = 0></i>'
+    ];
+    var imgContainer2 = [
+        '<i class="far fa-angry" value = 1></i>',
+        '<i class="fas fa-kaaba" value = 2></i>',
+        '<i class="fas fa-bicycle" value = 3></i>',
+        '<i class="fas fa-car-side" value = 4></i>',
+        '<i class="fas fa-cat" value = 5></i>',
+        '<i class="fas fa-child" value = 6></i>',
+        '<i class="fas fa-dollar-sign" value = 7></i>',
+        '<i class="far fa-grin-beam-sweat" value = 8></i>',
+        '<i class="far fa-heart" value = 9></i>',
+        '<i class="fas fa-horse" value = 0></i>'
+    ];
+
+    var number1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var number2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // imgContainer = imgContainer.concat(imgContainer);
+    //number = number.concat(number);
 
 
-for (let i = 0; i < 2; i++) {
-    //let rand = 0;//Math.floor(Math.random() * 2);
-    //if (rand === 1) {
-    if (imgContainer1.length) {
-        imgContainer1Call(i);
-        console.log("error1");
-    }  else {
-        console.log("error3");
-
+    function imgContainer1Call(i) {
+        let boxImge = Math.floor(Math.random() * imgContainer1.length);
+        squirSelctor[i].innerHTML = imgContainer1[boxImge];
+        //squirSelctor[i].value = boxImge;
+        //squirSelctor[i].style.color = "black";
+        imgContainer1.splice(boxImge, 1);
+        number1.splice(boxImge, 1);
+    }
+    function imgContainer2Call(i) {
+        let boxImge = Math.floor(Math.random() * imgContainer2.length);
+        squirSelctor[i].innerHTML = imgContainer2[boxImge];
+        //squirSelctor[i].value = boxImge;
+        //squirSelctor[i].style.color = "black";
+        imgContainer2.splice(boxImge, 1);
+        number2.splice(boxImge, 1);
     }
 
-    //     } else {
-    //         if (!imgContainer2.length) {
-    //             imgContainer2Call(i);
-    //         }else{
-    //             imgContainer1Call(i);
-    //         }
-    //     }
+
+    for (let i = 0; i < 20; i++) {
+        let rand = Math.floor(Math.random() * 2);
+        if (rand === 1) {
+            if (imgContainer1.length) {
+                imgContainer1Call(i);
+            } else {
+                imgContainer2Call(i);
+            }
+        } else {
+            if (imgContainer2.length) {
+                imgContainer2Call(i);
+
+            } else {
+                imgContainer1Call(i);
+            }
+        }
     }
 
     var f;
+    var ff = [];
     var s;
     var clickNo = 1;
-    squirSelctor.forEach(function (selectedBox1) {
-        selectedBox1.addEventListener("click", function () {
+
+    var fontawesome = document.querySelectorAll("i");
+    //console.log(fontawesome[1].value);
+    //squirSelctor.forEach(function (selectedBox) {
+    for (let i = 0; i < squirSelctor.length; i++) {
+        squirSelctor[i].addEventListener("click", function () {
             if (clickNo === 1) {
-                f = this.value;
-                //f.style.color = "#fff";
+                f = fontawesome[i].className;
+                ff.push(this);
+                console.log(f);
                 clickNo = 2;
             } else {
-                s = this.value;
-                //s.style.color = "red";
+                s = fontawesome[i].className;
+                ff.push(this);
+                console.log(s);
                 clickNo = 1;
+
                 if (f === s) {
-                    console.log(f + "  -1-  " + s);
-                    // f.style.color = "pink";
-                    // s.style.color = "pink";
+                    ff[0].style.background = "red";
+                    ff[1].style.background = "red";
+                    ff = [];
                 } else {
-                    console.log(f + "  -2-  " + s);
-                    // f.style.color = "rgb(24, 24, 24)";
-                    // s.style.color = "rgb(24, 24, 24)";
+                    console.log(f + "  -Wrong-  " + s);
+                    ff = [];
                 }
-                /* جاري تجربه 
-                 document.querySelectorAll
-                 */
             }
 
         });
+    };
 
-    });
 
-
-/*
-<i class="fas fa-bicycle"></i>
-<i class="fas fa-kaaba"></i>
-<i class="far fa-angry"></i>
-<i class="fas fa-car-side"></i>
-<i class="fas fa-cat"></i>
-<i class="fas fa-child"></i>
-<i class="fas fa-dollar-sign"></i>
-<i class="far fa-grin-beam-sweat"></i>
-<i class="far fa-heart"></i>
-<i class="fas fa-horse"></i>
-*/
+    /*
+    <i class="fas fa-bicycle"></i>
+    <i class="fas fa-kaaba"></i>
+    <i class="far fa-angry"></i>
+    <i class="fas fa-car-side"></i>
+    <i class="fas fa-cat"></i>
+    <i class="fas fa-child"></i>
+    <i class="fas fa-dollar-sign"></i>
+    <i class="far fa-grin-beam-sweat"></i>
+    <i class="far fa-heart"></i>
+    <i class="fas fa-horse"></i>
+    */
+}
