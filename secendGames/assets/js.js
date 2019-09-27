2ff37c966242e2ebcd2833854afa86885960cdb1
Abdulsalam <salwom@live.com>
2019-09-27 13:09:45 +0300

    before all cheng

--- secendGames/assets/js.js
+++ secendGames/assets/js.js
@@ -8,28 +8,28 @@ window.onload = function () {
 
 
     var imgContainer1 = [
-        '<i class="far fa-angry" value = 1></i>',
-        '<i class="fas fa-kaaba" value = 2></i>',
-        '<i class="fas fa-bicycle" value = 3></i>',
-        '<i class="fas fa-car-side" value = 4></i>',
-        '<i class="fas fa-cat" value = 5></i>',
-        '<i class="fas fa-child" value = 6></i>',
-        '<i class="fas fa-dollar-sign" value = 7></i>',
-        '<i class="far fa-grin-beam-sweat" value = 8></i>',
-        '<i class="far fa-heart" value = 9></i>',
-        '<i class="fas fa-horse" value = 0></i>'
+        '<i class="far fa-angry"></i>',
+        '<i class="fas fa-kaaba"></i>',
+        '<i class="fas fa-bicycle" ></i>',
+        '<i class="fas fa-car-side" ></i>',
+        '<i class="fas fa-cat" ></i>',
+        '<i class="fas fa-child" ></i>',
+        '<i class="fas fa-dollar-sign" ></i>',
+        '<i class="far fa-grin-beam-sweat" ></i>',
+        '<i class="far fa-heart" ></i>',
+        '<i class="fas fa-horse" ></i>'
     ];
     var imgContainer2 = [
-        '<i class="far fa-angry" value = 1></i>',
-        '<i class="fas fa-kaaba" value = 2></i>',
-        '<i class="fas fa-bicycle" value = 3></i>',
-        '<i class="fas fa-car-side" value = 4></i>',
-        '<i class="fas fa-cat" value = 5></i>',
-        '<i class="fas fa-child" value = 6></i>',
-        '<i class="fas fa-dollar-sign" value = 7></i>',
-        '<i class="far fa-grin-beam-sweat" value = 8></i>',
-        '<i class="far fa-heart" value = 9></i>',
-        '<i class="fas fa-horse" value = 0></i>'
+        '<i class="far fa-angry" ></i>',
+        '<i class="fas fa-kaaba" ></i>',
+        '<i class="fas fa-bicycle" ></i>',
+        '<i class="fas fa-car-side" ></i>',
+        '<i class="fas fa-cat" ></i>',
+        '<i class="fas fa-child" ></i>',
+        '<i class="fas fa-dollar-sign" ></i>',
+        '<i class="far fa-grin-beam-sweat"></i>',
+        '<i class="far fa-heart" ></i>',
+        '<i class="fas fa-horse" ></i>'
     ];
 
     var number1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -55,6 +55,10 @@ window.onload = function () {
         number2.splice(boxImge, 1);
     }
 
+    for (let i = 1; i < squirSelctor.length + 1; i++) {
+        squirSelctor[i - 1].value = i;
+
+    }
 
     for (let i = 0; i < 20; i++) {
         let rand = Math.floor(Math.random() * 2);
@@ -74,51 +78,97 @@ window.onload = function () {
         }
     }
 
-    var f;
-    var ff = [];
-    var s;
+    //     var firstValue;
+    //     var arrValue = [];
+    //     var secondValue;
+    //     var clickNo = 1;
+
+    //     var fontawesome = document.querySelectorAll("i");
+    //     //console.log(fontawesome[1].value);
+    //     //squirSelctor.forEach(function (selectedBox) {
+    //     for (let i = 0; i < squirSelctor.length; i++) {
+    //         squirSelctor[i].addEventListener("click", function () {
+    //             if (clickNo === 1) {
+    //                     firstValue = fontawesome[i].className;
+    //                     console.log(arrValue);
+    //                     arrValue.push(this);
+    //                     console.log(arrValue.length + " onw");
+    //                     clickNo = 2;
+    //             } else {
+    //                 if (arrValue[0].valu <=1) {
+    //                     secondValue = fontawesome[i].className;
+    //                     arrValue.push(this);
+    //                     clickNo = 1;
+
+    //                     if (firstValue === secondValue) {
+    //                         arrValue[0].style.background = "red";
+    //                         arrValue[1].style.background = "red";
+
+    //                     } else {
+    //                         console.log(firstValue + "  -Wrong-  " + secondValue);
+    //                     }
+    //                 }
+    //                 else {
+    //                     alert("Get onther");
+    //                 }
+    //                 arrValue = [];
+    //             }
+
+    //         });
+    //     };
+
+
+    //     /*
+    //     <i class="fas fa-bicycle"></i>
+    //     <i class="fas fa-kaaba"></i>
+    //     <i class="far fa-angry"></i>
+    //     <i class="fas fa-car-side"></i>
+    //     <i class="fas fa-cat"></i>
+    //     <i class="fas fa-child"></i>
+    //     <i class="fas fa-dollar-sign"></i>
+    //     <i class="far fa-grin-beam-sweat"></i>
+    //     <i class="far fa-heart"></i>
+    //     <i class="fas fa-horse"></i>
+    //     */
+
+    var firstValue;
+    var arrValue = [];
+    var secondValue;
     var clickNo = 1;
+    var values = 0;
+    const clicked1 =d ;
+    const clicked2 =dd ;
 
     var fontawesome = document.querySelectorAll("i");
-    //console.log(fontawesome[1].value);
-    //squirSelctor.forEach(function (selectedBox) {
     for (let i = 0; i < squirSelctor.length; i++) {
         squirSelctor[i].addEventListener("click", function () {
-            if (clickNo === 1) {
-                f = fontawesome[i].className;
-                ff.push(this);
-                console.log(f);
-                clickNo = 2;
+            const clicked1 =squirSelctor[i].value;
+            if (!clicked1) {
+                //console.log(this.value + " True " + clicked1);
+                clicked1 = squirSelctor[i].value;
+                firstValue = fontawesome[i].className;
+                squirSelctor[i].style.color = "red";
+                arrValue.push(firstValue);
+                console.log(firstValue + " clicked1");
+
+            } else if (!clicked2) {
+                clicked2 = squirSelctor[i].value;
+                secondValue = fontawesome[i].className;
+                arrValue.push(this);
+                squirSelctor[i].style.color = "green";
+                console.log(secondValue + " clicked2");
+
             } else {
-                s = fontawesome[i].className;
-                ff.push(this);
-                console.log(s);
-                clickNo = 1;
-
-                if (f === s) {
-                    ff[0].style.background = "red";
-                    ff[1].style.background = "red";
-                    ff = [];
-                } else {
-                    console.log(f + "  -Wrong-  " + s);
-                    ff = [];
+                for (let i = 0; i < squirSelctor.length; i++) { }
+                if (squirSelctor[i].value == clicked1) {
+                    console.log(squirSelctor[i].value +" " +clicked1);
+                    squirSelctor[i].style.color = "#fff";
+                } if (squirSelctor[i].value == clicked2) {
+                    squirSelctor[i].style.color = "#fff";
                 }
+                clicked1 = undefined;
+                clicked2 = undefined;
             }
-
         });
-    };
-
-
-    /*
-    <i class="fas fa-bicycle"></i>
-    <i class="fas fa-kaaba"></i>
-    <i class="far fa-angry"></i>
-    <i class="fas fa-car-side"></i>
-    <i class="fas fa-cat"></i>
-    <i class="fas fa-child"></i>
-    <i class="fas fa-dollar-sign"></i>
-    <i class="far fa-grin-beam-sweat"></i>
-    <i class="far fa-heart"></i>
-    <i class="fas fa-horse"></i>
-    */
+    }
 }
